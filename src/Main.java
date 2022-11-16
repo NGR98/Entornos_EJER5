5f275cf256ecca485b452110ab41a420a8895e7d
Nicolas <NicoGilRuiz@gmail.com>
2022-11-16 18:45:04 +0100

    "Cambio a private"

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println(veredicto(args[1], args[2]));
     }
 
-    public static String veredicto (String valor1, String valor2){
+    private static String veredicto (String valor1, String valor2){
             if (valor1.equals(valor2)) {
                 return ("Estudias 1 hora por modulo");
             } else if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
